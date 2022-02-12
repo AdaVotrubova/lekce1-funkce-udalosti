fc288fb75144bd4acd93b09d24e42217ab10a4ca
AdaVotrubova <99287593+AdaVotrubova@users.noreply.github.com>
2022-02-13 00:56:10 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
 // tady je místo pro náš program
 
-// 1. JavaScriptem změnit barvu nadpisu.
-//document.getElementById('js-main-heading').style.color = '#006400';
-document.querySelector('#js-main-heading').style.color = '#006400';
+let jmeno = "Ada";
+let vek = "39";
 
-// 2. Změna dekorace odstace. (Poznámka: Zde slouží pro demonstraci, ale obecně je lepší tyto jednoduché změny a úpravy dělat pomocí CSS!)
-let paragraphs = document.getElementsByClassName('js-paragraph');
-let firstParagraph = paragraphs[0];
-// Pomocí následujícího příkazu si mohu vypsat prvek do konzole. Vhodé při ladění programu.
-// console.log(paragraph);
-firstParagraph.style.fontSize = '26px';
-// Pomocí následujícího příkazu si mohu vypsat nastavenou velikost písma.
-//console.log(firstParagraph.style.fontSize);
-firstParagraph.style.fontStyle = 'italic';
-firstParagraph.style.fontWeight = 'bold';
+let nadpis = document.querySelector('h1');
+nadpis.style.color =  'red';
 
-// 3. Změna obrázku.
-let animalImageElement = document.getElementsByTagName('img')[0];
-animalImageElement.setAttribute('src', 'pes.jpg');
-animalImageElement.setAttribute('alt', 'Silný pes');
+let odstavec = document.querySelector("h2")
+odstavec.style.fontSize = "5px";
 
-// 4. Přemístění a otočení čtverečku
-let squareElement = document.querySelector('.ctverecek');
-document.querySelector('body').insertBefore(squareElement, document.querySelector('#js-main-heading'));
-// Pomocí následujícího příkazu přesunu čteveček ještě do paragrafu
-//firstParagraph.appendChild(squareElement);
-squareElement.style.transform = 'rotate(-90deg)';
+document.querySelector('img').src = 'pes.jpg';
+
+let squareElement = document.querySelector(".ctverecek");
+squareElement.style.transform = 'rotate(180deg)';
